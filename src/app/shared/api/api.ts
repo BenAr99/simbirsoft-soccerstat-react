11f286d77ec1dac869b41473fba4432216ed1cd5
BenAr99 <getto.tawers1344@gmail.com>
2026-04-26 18:33:48 +0400

    CORS fix

--- src/app/shared/api/api.ts
+++ src/app/shared/api/api.ts
@@ -1,7 +1,8 @@
 const BASE_URL = "https://api.football-data.org/v4";
+const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
 export const apiFetch = async <T>(url: string): Promise<T> => {
-    const res = await fetch(`${BASE_URL}${url}`, {
+    const res = await fetch(`${corsProxyUrl}${BASE_URL}${url}`, {
         headers: {
             "X-Auth-Token": import.meta.env.VITE_API_KEY
         },
